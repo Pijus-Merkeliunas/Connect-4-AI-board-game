dada5e291becd92c032c6d7673bdc00634f4267e
Pijus-Merkeliunas <151440050+Pijus-Merkeliunas@users.noreply.github.com>
2023-12-06 14:46:48 +0200

    Initial commit

---
+++ Connect4.h
@@ -0,0 +1,38 @@
+#include <windows.h>
+
+#define IDR_MYMENU 1001
+#define ID_FILE_EXIT 1002
+#define OPVP 1003
+#define PPVP 1004
+#define OAI 1005
+#define PAI 1006
+#define ID_RULES 1007
+
+#define IDI_ICON1 1010
+#define IDI_ICON2 1011
+#define IDI_ICON3 1012
+#define IDI_ICON4 1013
+
+#define IDI_ICON5 1014
+#define IDI_ICON6 1015
+#define IDI_ICON7 1016
+#define IDI_ICON8 1017
+#define IDI_ICON9 1018
+#define IDI_ICON10 1019
+#define IDI_ICON11 1020
+#define IDI_ICON12 1021
+
+#define P1P1 1050
+#define P1P2 1051
+#define P1P3 1052
+#define P1P4 1053
+#define P1P5 1054
+#define P1P6 1055
+#define P1P7 1056
+#define P2P1 1057
+#define P2P2 1058
+#define P2P3 1059
+#define P2P4 1060
+#define P2P5 1061
+#define P2P6 1062
+#define P2P7 1063
